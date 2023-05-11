746498bb0f63277edd3702595f651d741d3147c0
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2023-05-11 13:18:50 +0200

    Comptes.py

--- comptes.py
+++ comptes.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 
-scripts = ['numeros2.py', "numeros3.py", "mails2.py", "mails.py"]
+scripts = ['numeros2.py', "numeros3.py", "mails.py"]
 
 i = 0
 while i < len(scripts):
